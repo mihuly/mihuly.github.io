01010f6df90534df7bd49d8532a968894c77f6bc
mihuly <85845123+mihuly@users.noreply.github.com>
2025-12-16 09:29:43 -0800

    Update mistyland-patch.js

--- mistyland-patch.js
+++ mistyland-patch.js
@@ -1,1 +1,26 @@
 console.log('mistyland-patch.js loaded');
+
+(function () {
+  function patchPersonalData() {
+    const unit = document.getElementById('personalDataUnit');
+    if (!unit) return;
+
+    const textBlock = unit.querySelector('div span');
+    if (!textBlock) return;
+
+    textBlock.innerHTML = `
+      Ставя отметку, я принимаю
+      <a href="http://mistyland.ru/rules" target="_blank">Правила проживания</a>
+      и
+      <a href="http://mistyland.ru/agreement" target="_blank">Пользовательское соглашение</a>.
+    `;
+  }
+
+  // ждём, пока виджет реально построит DOM
+  const timer = setInterval(() => {
+    if (document.getElementById('personalDataUnit')) {
+      patchPersonalData();
+      clearInterval(timer);
+    }
+  }, 200);
+})();
